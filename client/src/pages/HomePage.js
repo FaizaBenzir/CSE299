26f56949f08000840cf38c5cc5b739c27933e35c
Faiza <107357199+FaizaBenzir@users.noreply.github.com>
2023-09-11 11:08:40 +0600

    Product and Category CRUD operation

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,13 +3,11 @@ import Layout from "./../components/Layout/Layout";
 import { useAuth } from "../context/auth";
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth()
+  const [auth, setAuth] = useAuth();
   return (
-    <Layout>
-      <div className="home">
-        <h1>HomePage</h1>
-        <pre>{JSON.stringify(auth, null, 4)}</pre>
-      </div>
+    <Layout title={"Best offers "}>
+      <h1>HomePage</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
   );
 };
