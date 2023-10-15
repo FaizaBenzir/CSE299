7861d7cfe35487b7ec2d7ea670e7ec2ac0d59e8a
Faiza <141565324+K-H-Shimul@users.noreply.github.com>
2023-10-15 12:35:57 +0600

    reviews

---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/cg";
+import "./Home.css";
+import Product from "./Product.js";
+import MetaData from "../layout/MetaData";
+import { getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const {loading, error, products, productsCount} = useSelector(state=>state.products)
+  useEffect(() => {
+    dispatch(getProduct());
+  }, [dispatch]);
+
+  return (
+    <Fragment>
+      <MetaData title="Grow Bangla" />
+          <div className="banner">
+            <p>Welcome to Grow Bangla</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="homeHeading">Featured Products</h2>
+
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => (
+                <Product product= {product} />
+              ))}
+          </div>
+        </Fragment>
+      )
+}
+
+export default Home;
